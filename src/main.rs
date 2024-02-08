aafb24e825d07eb546eeccee9aea7c33d04ee73c
Andrija Jelenkovic <andrija.jelenkovic@protonmail.com>
2024-02-08 19:41:07 +0100

    Add page table support

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,33 @@
 #![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use rust_os::{hlt_loop, println};
+use rust_os::{
+    hlt_loop,
+    memory::{self, BootInfoFrameAllocator},
+    println,
+};
+use x86_64::{
+    structures::paging::{Page, Translate},
+    VirtAddr,
+};
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
 
     rust_os::init();
+
+    let physical_memory_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(physical_memory_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     #[cfg(test)]
     test_main();
